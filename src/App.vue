00291325e5f90b3288b4840a3dbd8c392f865322
赵子晗 <15802920568@163.com>
2021-02-20 16:58:11 +0800

    模块化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>测试</p>
+    <!-- <p>测试</p>
     <p>{{$store.state.count}} {{$store.state.age}} {{$store.state.name}}</p>
     <p>计算属性优化</p>
     <p>{{count}} {{age}} {{name}}</p>
@@ -10,41 +10,48 @@
 
     <button @click="addCount(3)">增加</button>
     <button @click="reduceCount(3)">减少</button>
-    <button @click="setAsyncCount(4)">延时3S异步增加</button>
+    <button @click="setAsyncCount(4)">延时3S异步增加</button> -->
+    <h2>模块化取值</h2>  
+    <p> {{$store.state.setting.name}}</p>
+    <h2>模块化后使用辅助函数</h2>
+    <p>token:{{token}}</p>
+    <p>username:{{username}}</p>
   </div>
 </template>
 
 <script>
 // 引入辅助函数
-import { mapState, mapGetters ,mapMutations,mapActions } from 'vuex'
+// import { mapState, mapGetters ,mapMutations,mapActions } from 'vuex'
+import {  mapGetters  } from 'vuex'
 export default {
 mounted(){
   // script内需要this
-  console.log(this.$store.state.count);
+  // console.log(this.$store.state.count);
 },
 methods:{
   // 直接调用mutations
   // addCount(){
   //    this.$store.commit('addCount', 2)  
   // },
-  ...mapMutations(['addCount','reduceCount']),// 通过辅助函数调用mutations
+  // ...mapMutations(['addCount','reduceCount']),// 通过辅助函数调用mutations
 
   // setAsyncCount(){
   //   this.$store.dispatch('setAsyncCount',1)  // 直接调用actions
   // }
 
-  ...mapActions(['setAsyncCount']),// 通过辅助函数调用actions
+  // ...mapActions(['setAsyncCount']),// 通过辅助函数调用actions
 },
 // 使用计算属性优化
 computed:{
   // count(){
   //   return this.$store.state.count
   // },  // 直接调用State
-  name(){
-    return this.$store.state.name
-  },
-  ...mapState(['count','age']),  // 通过辅助函数调用State
-  ...mapGetters(['addNum','addr']) //通过辅助函数调用Getter
+  // name(){
+  //   return this.$store.state.name
+  // },
+  // ...mapState(['count','age']),  // 通过辅助函数调用State
+  // ...mapGetters(['addNum','addr']) //通过辅助函数调用Getter
+  ...mapGetters(['token','username'])
 }
 
 
--- src/main.js
+++ src/main.js
@@ -7,49 +7,79 @@ Vue.config.productionTip = false
 //引入、注册、创建实例
 import Vuex from 'vuex'
 Vue.use(Vuex)
+
+// 写法
+// ```
+// // const store = new Vuex.Store({
+// //   // 开启严格模式,只能在mutation中修改state
+// //   strict:true,
+// //   // state对象 公共状态属性,存放数据
+// //   state:{
+// //     count:1,
+// //     age:18,
+// //     name:'haha',
+// //     addr:{
+// //       guangdong:{
+// //         capital:'广州'     
+// //       }
+// //     } 
+// //   },
+// //   getters:{
+// //     addNum(state){
+// //       return state.count+state.age
+// //     },
+// //     // 改变state中数据的嵌套层级
+// //     addr(state){
+// //       return state.addr.guangdong.capital
+// //     }
+// //   },
+// //   // mutation对象,里面的每个属性都是一个可以改变state的函数
+// //   mutations:{
+// //     addCount(state,payload){
+// //       state.count+=payload
+// //     },
+// //     reduceCount(state,payload){
+// //       state.count-=payload
+// //     }
+// //   },
+// //   // actions对象 里面的属性都是可执行异步操作的函数
+// //   actions:{
+// //     setAsyncCount(store,payload){
+// //       setTimeout(() => {
+// //         store.commit('addCount', payload)
+// //       }, 3000);
+// //     }
+// //   }
+// // })
+// ```
+
+
+
+// module模块化
 const store = new Vuex.Store({
-  // 开启严格模式,只能在mutation中修改state
-  strict:true,
-  // state对象 公共状态属性,存放数据
-  state:{
-    count:1,
-    age:18,
-    name:'haha',
-    addr:{
-      china:{
-        guangdong:{city:'guangzhou'}      
-      }
-    } 
-  },
-  getters:{
-    addNum(state){
-      return state.count+state.age
-    },
-    // 改变state中数据的嵌套层级
-    addr(state){
-      return state.addr.china.guangdong.city
+  // 模块化
+  modules:{
+  user:{
+    state: {
+      token:'asfmgkdfgdsfb',
+      username:'大锤'
     }
   },
-  // mutation对象,里面的每个属性都是一个可以改变state的函数
-  mutations:{
-    addCount(state,payload){
-      state.count+=payload
-    },
-    reduceCount(state,payload){
-      state.count-=payload
-    }
-  },
-  // actions对象 里面的属性都是可执行异步操作的函数
-  actions:{
-    setAsyncCount(store,payload){
-      setTimeout(() => {
-        store.commit('addCount', payload)
-      }, 3000);
+  setting:{
+    state: {
+      name:'头条'
     }
   }
+},
+  getters:{
+    // 完整写法
+    token:function(state){
+      return state.user.token
+    },
+    // 箭头函数简写
+    username:state => state.user.username
+  }
 })
-
-
 
 new Vue({
   store,
