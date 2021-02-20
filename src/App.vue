45f58631c78325914557d1b5fa9fdde9a62681d1
赵子晗 <15802920568@163.com>
2021-02-20 12:26:25 +0800

    Vuex基础

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>测试</p>
+    <p>{{$store.state.count}} {{$store.state.age}} {{$store.state.name}}</p>
+    <p>计算属性优化</p>
+    <p>{{count}} {{age}} {{name}}</p>
+    <p>使用getter</p>
+    <p>和：{{addNum}}</p>
+    <p>地址：{{addr}}</p>
+
+    <button @click="addCount(3)">增加</button>
+    <button @click="reduceCount(3)">减少</button>
+    <button @click="setAsyncCount(4)">延时3S异步增加</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 引入辅助函数
+import { mapState, mapGetters ,mapMutations,mapActions } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+mounted(){
+  // script内需要this
+  console.log(this.$store.state.count);
+},
+methods:{
+  // 直接调用mutations
+  // addCount(){
+  //    this.$store.commit('addCount', 2)  
+  // },
+  ...mapMutations(['addCount','reduceCount']),// 通过辅助函数调用mutations
+
+  // setAsyncCount(){
+  //   this.$store.dispatch('setAsyncCount',1)  // 直接调用actions
+  // }
+
+  ...mapActions(['setAsyncCount']),// 通过辅助函数调用actions
+},
+// 使用计算属性优化
+computed:{
+  // count(){
+  //   return this.$store.state.count
+  // },  // 直接调用State
+  name(){
+    return this.$store.state.name
+  },
+  ...mapState(['count','age']),  // 通过辅助函数调用State
+  ...mapGetters(['addNum','addr']) //通过辅助函数调用Getter
+}
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,55 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// vuex 初始化
+//引入、注册、创建实例
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  // 开启严格模式,只能在mutation中修改state
+  strict:true,
+  // state对象 公共状态属性,存放数据
+  state:{
+    count:1,
+    age:18,
+    name:'haha',
+    addr:{
+      china:{
+        guangdong:{city:'guangzhou'}      
+      }
+    } 
+  },
+  getters:{
+    addNum(state){
+      return state.count+state.age
+    },
+    // 改变state中数据的嵌套层级
+    addr(state){
+      return state.addr.china.guangdong.city
+    }
+  },
+  // mutation对象,里面的每个属性都是一个可以改变state的函数
+  mutations:{
+    addCount(state,payload){
+      state.count+=payload
+    },
+    reduceCount(state,payload){
+      state.count-=payload
+    }
+  },
+  // actions对象 里面的属性都是可执行异步操作的函数
+  actions:{
+    setAsyncCount(store,payload){
+      setTimeout(() => {
+        store.commit('addCount', payload)
+      }, 3000);
+    }
+  }
+})
+
+
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
